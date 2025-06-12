544784dceaec0dfe58bd12c479369d2b2937587e
aditi-pnut <aditi@personalnutrition.com>
2025-06-12 17:59:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,6 @@ set -e
 
 echo "Starting deployment process..."
 
-# Login to ECR
-echo "Logging into ECR..."
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 878239241975.dkr.ecr.us-east-2.amazonaws.com
-
 # Change to the correct directory
 echo "Changing to project directory..."
 cd /home/ubuntu/pnut-code/
@@ -17,16 +13,17 @@ cd /home/ubuntu/pnut-code/
 echo "Stopping existing containers..."
 docker compose down
 
-# Pull new images (ignore failures to allow partial updates)
+
 echo "Pulling new images..."
-docker compose pull --ignore-pull-failures
+docker compose pull
 
 # Start containers using the latest images
 echo "Starting containers with latest images..."
-docker compose up -d --pull always
+docker compose up -d
 
 # Clean up unused images
-echo "Cleaning up unused images..."
-docker system prune -f
+echo "Cleaning up unused images & containers..."
+docker image prune -f
+docker container prune -f
 
 echo "Deployment completed!"
